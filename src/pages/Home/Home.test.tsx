0c4ab765afcb506736087cd43bcc2d94c2ec14e2
korkeng <Chawanan.akk@mail.kmutt.ac.th>
2020-11-20 00:31:57 +0700

    Clean unused code and prepare home screen

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from './Home';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Home />);
+  const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
